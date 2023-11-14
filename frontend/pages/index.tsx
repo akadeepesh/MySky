12414e55f561348c1750dd16d1739ae555ae4927
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-14 05:42:56 +0530

    fixed home page

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import HomeNav from "@/components/HomeNav";
+import Home from "@/components/Home";
+import Head from "next/head";
+const HomePage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>The Sky</title>
+      </Head>
+      <div className="flex justify-center items-center border-b border-gray-600 mx-2">
+        <HomeNav />
+      </div>
+      <Home />
+    </div>
+  );
+};
+
+export default HomePage;
