dbdc7cfffac40d66bcbc89230e58583fbc53a652
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-16 01:54:00 +0530

    adding to cors headers

--- backend/fav/fav/settings.py
+++ backend/fav/fav/settings.py
@@ -40,6 +40,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://my-sky-seven.vercel.app",
+    "mysky-production.up.railway.app",
 ]
 
 CORS_ALLOWED_CREDENTIALS = True
