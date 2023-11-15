390845396d99af39388a2a8ee2921d2d544cf1a4
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-16 03:02:26 +0530

    added button and router

--- frontend/components/HomeNav.tsx
+++ frontend/components/HomeNav.tsx
@@ -1,16 +1,37 @@
-import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "./ui/button";
+import { useRouter } from "next/router";
 
 const LandingNav = () => {
+  const router = useRouter();
+
+  const handleclick = () => {
+    if (router.pathname === "/") {
+      router.push("/favorites");
+    } else if (router.pathname === "/favorites") {
+      router.push("/");
+    }
+  };
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
   return (
     <nav className="flex md:mt-2 justify-between h-20 items-center w-screen">
-      <Link href="/">
-        <div className="font-bold text-xl md:text-2xl mx-1 md:mx-4 hover:text-slate-200 duration-200">
-          The Sky
-        </div>
-      </Link>
+      <div
+        onClick={handleLogoClick}
+        className="font-bold text-xl md:text-2xl mx-1 md:mx-4 hover:text-slate-200 duration-200 cursor-pointer"
+      >
+        The Sky
+      </div>
       <div className="flex justify-end items-center gap-3">
+        <span className="mr-6 md:mr-8 sm:mr-6 lg:mr-10">
+          <Button variant="outline" onClick={handleclick}>
+            {router.pathname === "/" ? "Favorites" : "Home"}
+          </Button>
+        </span>
         <ThemeToggle className="" />
         <UserButton afterSignOutUrl="/" />
       </div>
