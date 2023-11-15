10d9f04f0c8717928f00c18a5ceb0fd9a3aa2550
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-16 01:19:01 +0530

    setup for railway

--- backend/fav/fav/settings.py
+++ backend/fav/fav/settings.py
@@ -30,12 +30,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "https://my-sky-seven.vercel.app",
-]
-
+# ALLOWED_HOSTS = [
+#     "127.0.0.1",
+#     "localhost",
+#     "https://my-sky-seven.vercel.app",
+# ]
+ALLOWED_HOST = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
