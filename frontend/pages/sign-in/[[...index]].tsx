f77ffffc448841d608c6b9baf81f2e57c36ffde5
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-11 14:15:35 +0530

    Update sign-in and sign-up pages, removed prisma

--- frontend/pages/sign-in/[[...index]].tsx
+++ frontend/pages/sign-in/[[...index]].tsx
@@ -2,7 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="text-lg mb-5">Please Sign In to access my poetries.</div>
       <SignIn />
     </div>
   );
--- frontend/pages/sign-up/[[...index]].tsx
+++ frontend/pages/sign-up/[[...index]].tsx
@@ -1,9 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return(
-        <div className="flex justify-center items-center h-screen">
-            <SignUp />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-lg mb-5">Please Sign Up to access my poetries.</div>
+      <SignUp />
+    </div>
+  );
 }
